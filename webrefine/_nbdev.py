d9fb9aa21cb358b823cad0815419242a9d31ee9d
Edward Ross <edward@skeptric.com>
2021-11-30 15:32:09 +1100

    Restructure query objects
    Make querying easier to use by constructing a query object that emits
    records with accessors for content and relevant metadata.

--- webrefine/_nbdev.py
+++ webrefine/_nbdev.py
@@ -2,27 +2,32 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"header_and_rows_to_dict": "01_cdx.ipynb",
-         "mimetypes_to_regex": "01_cdx.ipynb",
-         "query_wayback_cdx": "01_cdx.ipynb",
-         "IA_CDX_URL": "01_cdx.ipynb",
-         "CaptureIndexRecord": "01_cdx.ipynb",
-         "fetch_internet_archive_content": "01_cdx.ipynb",
-         "make_session": "01_cdx.ipynb",
-         "WaybackResult": "01_cdx.ipynb",
-         "WaybackMachine": "01_cdx.ipynb",
-         "get_cc_indexes": "01_cdx.ipynb",
-         "jsonl_loads": "01_cdx.ipynb",
-         "query_cc_cdx_num_pages": "01_cdx.ipynb",
-         "query_cc_cdx_page": "01_cdx.ipynb",
-         "fetch_cc": "01_cdx.ipynb",
-         "CC_DATA_URL": "01_cdx.ipynb",
-         "CommonCrawl": "01_cdx.ipynb",
-         "get_warc_metadata": "02_warc.ipynb",
-         "QueryWARC": "02_warc.ipynb"}
+index = {"WarcFileRecord": "01_query.ipynb",
+         "WarcFileQuery": "01_query.ipynb",
+         "header_and_rows_to_dict": "01_query.ipynb",
+         "mimetypes_to_regex": "01_query.ipynb",
+         "query_wayback_cdx": "01_query.ipynb",
+         "IA_CDX_URL": "01_query.ipynb",
+         "CaptureIndexRecord": "01_query.ipynb",
+         "fetch_wayback_content": "01_query.ipynb",
+         "WaybackRecord": "01_query.ipynb",
+         "WaybackQuery": "01_query.ipynb",
+         "make_session": "01_query.ipynb",
+         "wayback_fetch_parallel": "01_query.ipynb",
+         "get_cc_indexes": "01_query.ipynb",
+         "parse_cc_crawl_date": "01_query.ipynb",
+         "cc_index_by_time": "01_query.ipynb",
+         "jsonl_loads": "01_query.ipynb",
+         "query_cc_cdx_num_pages": "01_query.ipynb",
+         "query_cc_cdx_page": "01_query.ipynb",
+         "CC_API_FILTER_BLACKLIST": "01_query.ipynb",
+         "fetch_cc": "01_query.ipynb",
+         "CC_DATA_URL": "01_query.ipynb",
+         "CommonCrawlRecord": "01_query.ipynb",
+         "CommonCrawlQuery": "01_query.ipynb"}
 
-modules = ["cdx.py",
-           "warc.py"]
+modules = ["core.py",
+           "query.py"]
 
 doc_url = "https://EdwardJRoss.github.io/webrefine/"
 
